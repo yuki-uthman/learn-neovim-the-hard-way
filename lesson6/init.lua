6bbaf3ec7b1bbe707e6fab740f4ef91dfeb205d7
Yuki Yoshimine <yuki07yuki@gmail.com>
2024-06-14 19:47:24 +0900

    add lesson 6

---
+++ lesson6/init.lua
@@ -0,0 +1,6 @@
+-- Set the colorscheme
+-- xvim.xcmd("xcolorscheme xdeusx")
+
+-- Use Ctrl-J and Ctrl-K to go up and down
+vim.keymap.set("c", "<C-J>", "<C-N>")
+vim.keymap.set("c", "<C-K>", "<C-P>")
